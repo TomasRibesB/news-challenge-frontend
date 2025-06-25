d3ec668e520b21a0badb671688c34e62dc820d55
Tomas Ribes <tomasribesb@gmail.com>
2025-06-25 02:40:42 -0300

    3. Genero componentes principales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { NewsList } from './news-list/news-list';
+import { NewsDetail } from './news-detail/news-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'news', pathMatch: 'full' },
+  {
+    path: 'news',
+    component: NewsList
+  },
+  {
+    path: 'news/:id',
+    component: NewsDetail
+  },
+  { path: '**', redirectTo: 'news' }
+];
